ac7b5889224141f4ccac9fd1a1e75dff86e5e6b8
Cosimo Palma <53081583+Glottocrisio@users.noreply.github.com>
2023-03-10 10:56:07 +0100

    Update SeleniumFirefox.py

--- SeleniumFirefox.py
+++ SeleniumFirefox.py
@@ -1,13 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+
+def python():
+    options = Options()
+    options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+    driver = webdriver.Firefox(executable_path=r'C:\Program Files\Mozilla Firefox\gecko\geckodriver.exe', options=options)
+    #driver.get('http://google.com/')
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element(By.NAME, "q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    #driver.close()
+
